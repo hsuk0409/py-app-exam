8973a74cb2e77c09421da58afdccde2aec468995
ChoinSuk <hsuk@thecommerce.co.kr>
2021-09-09 10:23:36 +0900

    리팩토링

--- func-study/bookstore.py
+++ func-study/bookstore.py
@@ -1,12 +1,10 @@
 class Book:
 
-    def setData(self, title, price):
-        self.title = title
-        self.price = price
-
     def printData(self):
         print('제목: ', self.title)
         print('가격: ', self.price + '원')
 
-    def __init__(self):
+    def __init__(self, title, price):
+        self.price = price
+        self.title = title
         print('책 개체 생성!')
